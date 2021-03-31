91e6baa9b6fd85f64b5240ea9d29422aec9c385f
Perry E <its_Q@hotmail.com>
2021-03-31 12:59:25 +0200

    Add project files.

---
+++ DataProcessor/Access.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Dapper;
+
+namespace DataProcessor
+{
+    public class Access
+    {
+        static string ConnectionString;
+
+        public Access(string _ConnectionString)
+        {
+            ConnectionString = _ConnectionString;
+        }
+
+        public static string GetConnectionString(string connectionName = "RepairShopDB")
+        {
+            return ConnectionString;
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+        public static int UpdateData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+        public static int DeleteData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+        public static int ExecuteDataString(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql);
+            }
+        }
+    }
+}
---
+++ DataProcessor/Main.cs
@@ -0,0 +1,67 @@
+﻿using QSS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+namespace DataProcessor
+{
+    public class StringValueAttribute : Attribute
+    {
+        #region Properties
+        public string StringValue { get; protected set; }
+        #endregion
+
+        #region Constructor
+        public StringValueAttribute(string value)
+        {
+            this.StringValue = value;
+        }
+        #endregion
+    }
+    public enum ValueSelectType
+    {
+        NamesAndValues,
+        Names,
+        Values
+    }
+
+    public enum cmdType
+    {
+        [StringValue("select")]
+        Select,
+        [StringValue("update")]
+        Update,
+        [StringValue("delete")]
+        Delete,
+        [StringValue("insert into")]
+        Insert
+    }
+
+    public static class Connection
+    {
+        
+        public static List<R> ListT<R,D>(D Data, string databaseTable, string[] excludeStrings = null)
+        {
+            if (Data == null || databaseTable == null || databaseTable.Length == 0)
+                return null;
+            return Access.LoadData<R>($"from dbo.select {SQLs.FormatString(Data.GetType().GetProperties(), excludeStrings)}");
+        }
+
+        public static R One<R, D>(D Data, string databaseTable, string[] excludeStrings = null)
+        {
+            return ListT<R, D>(Data, databaseTable, excludeStrings)[0];
+        }
+
+        public static int Update<D>(D Data, string databaseTable, string[] wheres, string[] wheresValues, string[] excludeStrings = null)
+        {
+            if (Data == null || databaseTable == null || databaseTable.Length == 0)
+                return 0;
+
+
+            return Access.ExecuteDataString($"update dbo.{databaseTable} set " +
+                $"{SQLs.FormatString(Data.GetType().GetProperties(), excludeStrings)} " +
+                $"{SQLs.WhereFormat(Data.GetType().GetProperties(), wheres, wheresValues)}");
+        }
+    }
+
+}
