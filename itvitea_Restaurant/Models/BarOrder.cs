65770eb46615c58c1a9212806c5ab2e17f73b5e3
Perry E <its_Q@hotmail.com>
2021-04-09 10:17:50 +0200

    added database

---
+++ itvitea_Restaurant/Models/BarOrder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace itvitea_Restaurant.Models
+{
+    public class BarOrder
+    {
+        public int id { get; set; }
+        public int tableId { get; set; }
+        public int drinkId { get; set; }
+    }
+}
